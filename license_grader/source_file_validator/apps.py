cd0542e6cf95b97f300942527f3df3d742020b95
krysnuvadga <tetechris20@gmail.com>
2017-07-05 13:53:43 +0100

    Removed unused files

--- license_grader/source_file_validator/apps.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class SourceFileValidatorConfig(AppConfig):
-    name = 'source_file_validator'
